4398f8e235a66f5ab57346b12bffbf5eb8bb786b
Senni Tan <sennitan@geotab.com>
2022-02-19 14:31:10 -0500

    merge

--- friday/src/App.js
+++ friday/src/App.js
@@ -8,6 +8,7 @@ import Error from './features/Error';
 // import Register from "./components/Authentication/Register";
 // import Reset from "./components/Authentication/Reset";
 // import Dashboard from "./components/Authentication/Dashboard";
+import Journal from './features/Journal';
 import JournalMain from './features/JournalMain';
 import Assessment from './features/Assessment';
 import About from './features/About'
@@ -21,6 +22,7 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/assessment" element={<Assessment />} />
           <Route path="/journal" element={<Journal />} />
+          <Route path="/journalmain" element={<JournalMain />} />
           {/* <Route path="/journal" element={<Journal />} />
           <Route path='/feedback' component={() => {
             window.location.href = 'https://google.com';
