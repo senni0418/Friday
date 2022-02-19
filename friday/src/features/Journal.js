f39565a06de010b9abe19fbf57f593905f7e70f5
Senni Tan <sennitan@geotab.com>
2022-02-19 13:12:23 -0500

    add button

--- friday/src/features/Journal.js
+++ friday/src/features/Journal.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import ResponsiveAppBar from '../components/Basic/js/ResponsiveAppBar';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import InputAdornment from '@mui/material/InputAdornment';
+import Box from '@mui/material/Box';
 
 const Journal = () => {
     const [text, setText] = useState('')
@@ -8,19 +11,24 @@ const Journal = () => {
     return (
         <>
             <ResponsiveAppBar/>
-            <div style={{display: 'flex', justifyContent:'center', marginTop: '100px', height: '100vh'}}>
-                <TextField
-                    placeholder="Start writing your daily journals"
-                    multiline
-                    rows={20}
-                    sx={{width: 1000}}
-                    value={text}
-                    onChange={(e) => {
-                        setText(e.target.value)
-                    }}
-                />
-
+            <div style={{display: 'flex',justifyContent:'center', marginTop: '100px'}}>
+                <Box>
+                    <TextField
+                        placeholder="Start writing your daily journals"
+                        multiline
+                        rows={20}
+                        sx={{width: 1000}}
+                        value={text}
+                        onChange={(e) => {
+                            setText(e.target.value)
+                        }}
+                    />
+                </Box>           
             </div>
+            <div style={{display: 'flex',justifyContent:'right', marginTop: '10px', marginRight: '410px'}}>
+                <Button>Button</Button>
+            </div>
+            <div>result here</div>
         </>
     );
 }
