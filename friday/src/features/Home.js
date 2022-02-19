32d7a0b18d5a23f5546f74b26a11451c3ce32bad
Manyi Cheng <chengm33@mcmaster.ca>
2022-02-19 11:52:02 -0500

    push

--- friday/src/features/Home.js
+++ friday/src/features/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ResponsiveAppBar from '../components/Basic/js/ResponsiveAppBar';
+import Footer from '../components/Basic/js/Footer';
 
 const Home = () => {
     return (
         <div>
             <ResponsiveAppBar/>
+
+            <Footer/>
         </div>
     );
 }
