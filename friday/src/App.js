34db8b4a3e22af0190bab2a4e70027b62ac885f2
Manyi Cheng <chengm33@mcmaster.ca>
2022-02-19 11:26:48 -0500

    update

--- friday/src/App.js
+++ friday/src/App.js
@@ -5,10 +5,9 @@ import './App.css';
 import Login from './components/Authentication/Login';
 import Home from './features/Home';
 import Error from './features/Error';
-import Register from "./components/Authentication/Register";
-import Reset from "./components/Authentication/Reset";
-import Dashboard from "./components/Authentication/Dashboard";
-import ResponsiveAppBar from './components/Basic/js/ResponsiveAppBar';
+// import Register from "./components/Authentication/Register";
+// import Reset from "./components/Authentication/Reset";
+// import Dashboard from "./components/Authentication/Dashboard";
 import Journal from './features/Journal';
 import Assessment from './features/Assessment';
 import About from './features/About'
@@ -17,20 +16,16 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login/>} />
-          <Route path="/app/home" element={<Home />} />
-          <Route path="/*" element={<Error/>} />
-          <Route path='/app/feedback' component={() => {
+          <Route path="/" element={<Home/>} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/assessment" element={<Assessment />} />
+          {/* <Route path="/journal" element={<Journal />} />
+          <Route path='/feedback' component={() => {
             window.location.href = 'https://google.com';
             return null;
           }}/>
-          <Route path="/" element={<Home/>} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/reset" element={<Reset />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
-          <Route path="/app/about" element={<About />} />
-          <Route path="/app/assessment" element={<Assessment />} />
-          <Route path="/app/journal" element={<Journal />} />
+          <Route path="/*" element={<Error/>} /> */}
       </Routes>
     </Router>
   );
